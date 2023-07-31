7eb21dc0b3eaf20318b3c513ff6e9f1001ffa0c4
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:07:17 +0500

    Q:12 From batch-46 assignment

---
+++ greeting.ts
@@ -0,0 +1,13 @@
+//q12: Greetings: Start with the array you used in Exercise 11, 
+// but instead of just printing each person’s name, 
+// print a message to them. The text of each message should be the same,
+// but each message should be personalized with the person’s name.
+
+let names = [
+    "Hello! Tahir, How are you?",
+    "Hello! Ahmed, How are you?",
+    "Hello! Amjad, How are you? "
+];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
