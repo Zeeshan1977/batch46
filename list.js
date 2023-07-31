6fa8af30f91bac86a7606b6c3ac4a8362f99a80b
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:14:02 +0500

    Q:20 From batch-46 assignment

--- list.js
+++ list.js
@@ -1,11 +1,14 @@
 "use strict";
-//q11: Names: Store the names of a few of your friends in a array called names.
-//Print each person’s name by accessing each element in the list, one at a time.
-let names = [
-    " Tahir!",
-    " Ahmed!",
-    " Amjad!"
-];
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
+//q20: Think of something you could store in a array. For example, you could make a list of mountains,
+// rivers, countries, cities, languages, or anything else you’d like. 
+//Write a program that creates a list containing these items.  
+var Mountains = "Nagaparbat,Himalya,K2";
+var Rivers = "Ravi, Satalaj,Chinab";
+var Countries = "Pakistan,China,Afghanistan";
+var Language = "Urdu,Sindhi,Pashto";
+var Cities = "Karachi,Lahore,Islamabad";
+console.log(Mountains);
+console.log(Rivers);
+console.log(Countries);
+console.log(Language);
+console.log(Cities);
