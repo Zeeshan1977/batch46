d89ba8840e86b645bb382035aef3eeaa2767b56b
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:12:11 +0500

    Q:18 From batch-46 assignment

---
+++ city.js
@@ -0,0 +1,9 @@
+"use strict";
+//q18 
+let places = ["Makkah", "Madina", "Damascus", "USA", "Europe"];
+console.log("Array is in origional order ", places);
+let tempArray;
+tempArray = places.sort();
+console.log("Temporary Array in sorted form ", places.sort());
+console.log("Places Array in sorted form ", places.sort());
+console.log("However still array in origional state : ", places);
