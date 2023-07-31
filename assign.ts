15579efb2c985781e0630abcbeec9f690bf99224
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:00:51 +0500

    Q:9 From batch-46 assignment

---
+++ assign.ts
@@ -0,0 +1,9 @@
+//q9: Favorite Number: Store your favorite number in a variable. 
+//Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+var a="8";
+const b="      Your Favorite Number is :   ";
+console.log (b+a);
+
+
+
