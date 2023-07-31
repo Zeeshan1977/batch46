86a964901fa06248459a9408fbdfcfa34da6a685
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:04:48 +0500

    q:10 FROM BATCH46 ASSIGNMENT

---
+++ addcoments.ts
@@ -0,0 +1,13 @@
+//q10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file.
+//Then write one sentence describing what the program does.
+
+
+console.log("This is Zeeshan below programe is written by me on 9th July2023,in that calculation all answere come Eight:");
+console.log("Addition:" + (5+3))
+console.log("Subtraction:" + (10-2))
+console.log("Multiplication:" + (4*2))
+console.log("Division:" + (16/2))
+console.log("This is Zeeshan below programe is written by me on 9th July2023,in that I am telling my Favorite number");
+const a="8";
+let b="      My favorite Number is Eight:   ";
+console.log (b+a);
