bba865767f37d6940084dd2334c29fd15bc92783
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:14:47 +0500

    Q:21 From batch-46 assignment

---
+++ objest.js
@@ -0,0 +1,10 @@
+"use strict";
+//q21:They think of something you could store in a TypeScript Object.
+// Write a program that creates Objects containing these items.
+let class1 = {
+    name: "Zeeshan Abbas",
+    year: 2016,
+    qualification: "Masters in Business Administration",
+};
+console.log(class1);
+console.log(`Mr. ${class1.name} got his ${class1.qualification} degree in the year ${class1.year}`);
