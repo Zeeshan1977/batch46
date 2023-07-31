b6d9b1eac5384988c9046c295dbc98dc2a1a7fc7
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:18:19 +0500

    Q:27 From batch-46 assignment

---
+++ color1.ts
@@ -0,0 +1,21 @@
+// Q27 Assignment No 01 Batch 46 Q1
+
+//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+
+//• If the alien is green, print a message that the player earned 5 points.
+
+
+let alien_color = 'green'
+
+if (alien_color === 'green') {
+console.log('The player earned 5 points')
+} else if 
+//• If the alien is yellow, print a message that the player earned 10 points.
+ (alien_color === 'yellow') {
+console.log('The player earned 10 points')
+} else if 
+//• If the alien is red, print a message that the player earned 15 points.
+(alien_color === 'red') {
+console.log('The player earned 15 points')
+} 
+//• Write three versions of this program, making sure each message is printed for the appropriate color alien.
