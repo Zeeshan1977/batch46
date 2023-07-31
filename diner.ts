aecb517e6d62d51d3a37140a52056af17700b03d
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:08:28 +0500

    Q:14 From batch-46 assignment

---
+++ diner.ts
@@ -0,0 +1,8 @@
+//q14: Guest List: If you could invite anyone, living or deceased, to dinner, 
+//who would you invite? Make a list that includes at least three people you’d like to invite to dinner.
+// Then use your list to print a message to each person, inviting them to dinner.
+const livigorDeceased :string[]= ["Faraz","Manni", "Mustansar"];
+console.log(`Mr. ${livigorDeceased[0]} You are invited for a dinner with us on 30 June 2023 at Lalqilla.`);
+console.log(`Mr. ${livigorDeceased[1]} You are invited for a dinner with us on 30 June 2023 at Lalqilla.`);
+console.log(`Mr. ${livigorDeceased[2]} You are invited for a dinner with us on 30 June 2023 at Lalqilla.`);
+
