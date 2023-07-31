97a93f599c015239f4a448ae38208064ebb86bb1
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 18:58:36 +0500

    Q:6 From batch-46 assignment

---
+++ Stripname.ts
@@ -0,0 +1,11 @@
+//Q6: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed.
+//Then print the name after striping the white spaces.
+
+
+console.log("\Stripingthename")
+let namewithWhitespace ="\t \n Zeeshan Abbas \n \t";
+console.log("Name with Whitespace:");
+console.log("$$" + namewithWhitespace + "$$");
+let namewithoutWhitespace = namewithWhitespace.trim();
+console.log("Name without Whitespace:");
+console.log("***" + namewithoutWhitespace + "***");
