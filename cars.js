10e0e026cac570052079c8d16f73c8e8623d13e5
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 21:57:31 +0500

    Q:45 From batch-46 assignment

---
+++ cars.js
@@ -0,0 +1,11 @@
+"use strict";
+var car = {
+    Manufacturer: "Toyota",
+    Model: "Yaris",
+    Year: "2022",
+    Color: "Altitude Black",
+    Engine: "v6 4cylinder",
+};
+//car.Manufacturer="Honda";
+console.log(car);
+console.log("mae te kabhi Honda na lae saan");
