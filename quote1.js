b0dc44fd7a61c8ae1f680957c4fdb8aaf0618c1a
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 18:57:04 +0500

    Q:4 From batch-46 assignment

---
+++ quote1.js
@@ -0,0 +1,6 @@
+"use strict";
+//Q4: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// Your output should  including the quotation marks
+const authoreName = "Albert Einstein!";
+const message = ('"A person who never made mistake never tried anything new."');
+console.log(authoreName, "once he said:" + message);
