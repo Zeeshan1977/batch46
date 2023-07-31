728e6a0efbc22848cae93daef049a9a0e61753ad
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 21:56:41 +0500

    Q:44 From batch-46 assignment

---
+++ sanwich.ts
@@ -0,0 +1,23 @@
+//Q44: Sandwiches: Write a function that accepts array of items a person wants on a sandwich.The function should have one parameter that collects as many item
+//as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number
+//of arguments each time.
+
+let sandwich_items={
+
+cheese: "extra",
+Tomatoes: "normal",
+Pickles: "no",
+Lettuce: "slightly",
+cacumber: "no",
+mayonnese: "Light",
+Ketchup: "yes",
+MyOrder:function sandwich_items(myOrder:string){
+    return(`${sandwich_items}`)}
+}
+
+
+//where I am not getting it?/i need help here
+
+
+
+
