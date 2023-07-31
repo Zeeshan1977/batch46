1b2b2732ff00dc7fe05fde8793a03048c2a15c84
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 21:56:09 +0500

    Q:43 From batch-46 assignment

---
+++ magi.js
@@ -0,0 +1,12 @@
+"use strict";
+//q43: Unchanged Magicians: Start with your work from Exercise 40. Call the
+// function make_great() with a copy of the array of magicians’ names. Because the
+// original array will be unchanged, return the new array and store it in a separate array.
+// Call show_magicians() with each array to show that you have one array of the original
+// names and one array with the Great added to each magician’s name.
+function make_great(magicians) {
+    return (`${make_great}`);
+}
+console.log(make_great("Asif"));
+console.log(make_great("Mustafa"));
+console.log(make_great("Shahid"));
