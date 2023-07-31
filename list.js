32293808d99c82d20996213439e403dd0e89fa62
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:05:32 +0500

    Q:11 From batch-46 assignment

---
+++ list.js
@@ -0,0 +1,11 @@
+"use strict";
+//q11: Names: Store the names of a few of your friends in a array called names.
+//Print each person’s name by accessing each element in the list, one at a time.
+let names = [
+    " Tahir!",
+    " Ahmed!",
+    " Amjad!"
+];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
