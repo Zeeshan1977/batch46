7fe8e805ed69e74324fdf16e24accf4c2c6a4173
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:20:30 +0500

    Q:29 From batch-46 assignment

---
+++ fruits.js
@@ -0,0 +1,17 @@
+"use strict";
+//q29 Fruits Description
+// Q29 Assignment No 01 Batch 46 Q1
+//Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+//• Make a array of your three favorite fruits and call it favorite_fruits.
+//• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, 
+//the if block should print a statement, such as You really like bananas!
+let favorite_fruits = ['Mango', 'Pineapple', 'Banana'];
+let frouits = ['Guawa', 'Grapes', 'Banana', 'Pears', 'Apple'];
+for (let a = 0; a < frouits.length; a++) {
+    for (let b = 0; b < favorite_fruits.length; b++) {
+        if (frouits[a] === favorite_fruits[b]) {
+            console.log(`I got your favorite fruit ${frouits[a]}`);
+            break;
+        }
+    }
+}
