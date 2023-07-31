84a9c043983eca40ceae393a146a2de6065d90c5
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-07-31 19:07:53 +0500

    Q:13 From batch-46 assignment

---
+++ tranport.js
@@ -0,0 +1,13 @@
+"use strict";
+//q13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car,
+// and make a list that stores several examples.
+// Use your list to print a series of statements about these items,
+// such as “I would like to own a Honda motorcycle.”
+var mod = "My Favourite mode of Transportion is 'Motor-car'";
+const a = "There are Honda, Kia, Toyota, Suzuki, Faw in market.";
+var b = "By quality wise I prefare Honda,";
+const c = "Thats why 'Mae te... Honda e lae saan'";
+console.log(mod);
+console.log(b);
+console.log(a);
+console.log(c);
