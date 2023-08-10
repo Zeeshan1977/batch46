18bbb8865608a40cdb09e712f09e329cd295e0e2
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-08-10 15:01:10 +0500

    Update cars.js

--- cars.js
+++ cars.js
@@ -1,11 +1,14 @@
 "use strict";
-var car = {
-    Manufacturer: "Toyota",
-    Model: "Yaris",
-    Year: "2022",
-    Color: "Altitude Black",
-    Engine: "v6 4cylinder",
-};
-//car.Manufacturer="Honda";
-console.log(car);
-console.log("mae te kabhi Honda na lae saan");
+
+function car1(Manufacturer, model, color, engine) {
+    let message = ` ${Manufacturer}, ${model}, is my favourite car for its ${color} color and ${engine} cylinder performance.`;
+    return message;
+}
+function car2(Manufacturer, model, color, engine) {
+    let message = ` ${Manufacturer}, ${model}, is my favourite car for its ${color} color and ${engine} cylinder performance.`;
+    return message;
+}
+const returnedMessage1 = car1("Toyota", "Yaris", "White", 6);
+const returnedMessage2 = car2("Honda", "Prelude", "Red", 8);
+console.log(returnedMessage1);
+console.log(returnedMessage2);
