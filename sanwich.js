1b2084edce273e0f5e571f0a856d98c68827d817
Zeeshan1977 <139474307+Zeeshan1977@users.noreply.github.com>
2023-08-10 16:29:12 +0500

    Update sanwich.js

--- sanwich.js
+++ sanwich.js
@@ -1,15 +1,15 @@
 //Q44: Sandwiches: Write a function that accepts array of items a person wants on a sandwich.The function should have one parameter that collects as many item
 //as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number
 //of arguments each time.
-var sandwich_items = {
-    cheese: "extra",
-    Tomatoes: "normal",
-    Pickles: "no",
-    Lettuce: "slightly",
-    cacumber: "no",
-    mayonnese: "Light",
-    Ketchup: "yes",
-    MyOrder: function sandwich_items(myOrder) {
-        return ("".concat(sandwich_items));
-    }
-};
+
+
+function sandWich(Items, Qty) {
+    var myOrder = "i want to order ".concat(Qty, "! ").concat(Items, "  with \"extra-cheese\",\"mayonese\",\"tomatoes\" please");
+    return myOrder;
+}
+var confirmOrder1 = sandWich("grill Chicken", 1);
+var confirmOrder2 = sandWich("Zinger", 2);
+var confirmOrder3 = sandWich("grilled Cheese", 4);
+console.log(confirmOrder1);
+console.log(confirmOrder2);
+console.log(confirmOrder3);
