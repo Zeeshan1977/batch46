ea67a861a69b0dbf54e47ee8c69505b99318aca3
Zeeshan <139474307+Zeeshan1977@users.noreply.github.com>
2023-11-14 19:54:22 +0500

    Add files via upload

---
+++ Calcula.ts
@@ -0,0 +1,51 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer : {
+   Operator : string,
+   numberOne : number,
+   numberTwo: number, 
+}
+ = await inquirer.prompt([
+   
+   {
+       type : "number",
+       name : "numberOne",
+       message : "Enter your first number"
+    },
+    {
+        type : "number",
+        name : "numberTwo",
+        message : "Enter your second number"
+     },
+     {
+        type : "list",
+        name :"Operator",
+        choices : ["+", "-", "/", "*"],
+        message : "Select Operators"
+     }
+]);
+
+console.log(answer.numberOne + answer.numberTwo)
+// const {numberOne,numberTwo,Operator}=answer;
+// if(numberOne && numberTwo && Operator){
+
+let result : number = 0;
+if (answer.Operator === "+"){
+   // console.log("typeof",answer.numberOne);
+   // console.log("typeof",);
+   // console.log(typeof(answer.numberOne))
+   // console.log(typeof(answer.numberTwo))   
+result = answer.numberOne + answer.numberTwo
+}
+else     if (answer.Operator === "-"){
+result = answer.numberOne - answer.numberTwo
+}
+else if (answer.Operator === "/"){
+result =answer.numberOne / answer.numberTwo
+}
+else if (answer.Operator === "*"){
+result =answer.numberOne * answer.numberTwo
+}
+console.log("Your result is: " , result, "Thank you for Zee calculator.");
+ 
+ 
